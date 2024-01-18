f250145ca3004f4dbf29fe22792a619a14fc7e14
soorajtembhurkar <surajtembhurkar358@gmail.com>
2024-01-18 20:52:51 +0530

    second commit

--- Day1.js
+++ Day1.js
@@ -1,3 +1,5 @@
 //A variable is a container for a value, like a number we might use in a sum, or a string that we might use as part of a sentence.
 
-var variagle = "hello";
+var variable = "hello";
+
+console.log(variable)
